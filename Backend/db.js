f07226ce85dc55caa265d08c56758ada3be40d86
Shubham Singh <shubhamkumar2452004@gmail.com>
2025-12-05 18:32:25 +0530

    improve ui

--- Backend/db.js
+++ Backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/MongoDB_2");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("MongoDB Error:", error);
--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,23 @@
+require("dotenv").config();
 const taskRoutes = require("./routes/taskRoutes");
 const { Task, connectDB } = require("./db");
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const { model } = require("mongoose");
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+
 app.use(express.json());
 app.use("/", taskRoutes);
 connectDB();
+// app.use(express.static(path.join(__dirname, "../dist")));
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../dist/index.html"));
+// });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
