087345a42c39fa55c7dce51d1a3760f844fd8a06
Shubham Singh <shubhamkumar2452004@gmail.com>
2025-12-06 14:02:01 +0530

    error resolve for deploy netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Main from "./components/Main";
 import TodoAi from "./components/TodoAi";
 
